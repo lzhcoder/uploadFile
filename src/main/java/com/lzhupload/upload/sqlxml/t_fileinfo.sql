48cce5c989baedb45a94e7355ac3fce73aea36a1
623196924@qq.com <623196924@qq.com>
2016-11-08 16:23:45 +0800

    sql文件

--- src/main/java/com/lzhupload/upload/sqlxml/t_fileinfo.sql
+++ src/main/java/com/lzhupload/upload/sqlxml/t_fileinfo.sql
@@ -1,26 +1,26 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : btkj
+Source Server         : botanndesign
 Source Server Version : 50616
 Source Host           : botannrds.mysql.rds.aliyuncs.com:3306
-Source Database       : botann_marketing_plt
+Source Database       : botanndesign
 
 Target Server Type    : MYSQL
 Target Server Version : 50616
 File Encoding         : 65001
 
-Date: 2016-04-27 17:11:10
+Date: 2016-11-08 16:22:39
 */
 
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
--- Table structure for tmk_fileinfo
+-- Table structure for t_fileinfo
 -- ----------------------------
-DROP TABLE IF EXISTS `tmk_fileinfo`;
-CREATE TABLE `tmk_fileinfo` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `t_fileinfo`;
+CREATE TABLE `t_fileinfo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `vid` varchar(128) DEFAULT NULL,
   `registerDate` date DEFAULT NULL,
   `uploadDate` date DEFAULT NULL,
@@ -32,9 +32,6 @@ CREATE TABLE `tmk_fileinfo` (
   `fileSize` int(11) DEFAULT '0',
   `note` varchar(256) DEFAULT NULL,
   `uploadIP` varchar(16) DEFAULT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of tmk_fileinfo
--- ----------------------------
+  `originName` varchar(255) DEFAULT NULL COMMENT '原始文件名',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1299 DEFAULT CHARSET=utf8;
